d3a6168f0db17e6e41840459a3919452d500b4f0
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-04-03 09:46:57 +0700

    okk

--- pages/blog/index.page.tsx
+++ pages/blog/index.page.tsx
@@ -21,7 +21,7 @@ export default function BlogPage() {
               type="button"
               className="inline-flex items-center w-32 h-12 px-8 text-base font-bold leading-6 text-white transition duration-150 ease-in-out bg-indigo-400 border border-transparent hover:bg-indigo-700 focus:outline-none active:bg-indigo-700"
             >
-              Sign Up
+              Sign Upp
             </button>
           </span>
         </div>
