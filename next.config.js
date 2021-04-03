eb594494c7298bd0bd5c13f3995f68af59b37bca
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-04-03 19:14:22 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -100,11 +100,14 @@ module.exports = withPlugins(
     [
       withPWA,
       {
-        pwa: {
-          disable: process.env.NODE_ENV === "development",
-          dest: "public"
-        }
+      pwa: {
+        disable: process.env.NODE_ENV === "development",
+        register: true,
+        scope: "/",
+        sw: "service-worker.js",
+        dest: "public"
       }
+    }
     ],
     [
       withCss,
